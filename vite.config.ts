50b01ec5b9acc0811fde796d54d6be4f512bd3eb
GregorisB <gregorbachts@gmail.com>
2024-09-08 23:05:05 +0300

    project setup

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: "build",
+  },
+  server: {
+    host: true,
+    open: true,
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  plugins: [react(), mkcert(), svgr(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
