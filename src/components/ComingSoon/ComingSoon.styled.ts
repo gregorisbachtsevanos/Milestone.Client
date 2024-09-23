120d8d3d52f645d4d7de9d6d70c3bb8854d607fa
GregorisB <gregorbachts@gmail.com>
2024-09-23 12:56:29 +0300

    "Update ComingSoon.styled.ts: changed container height from 50vh to 80vh and added hover effects"

--- src/components/ComingSoon/ComingSoon.styled.ts
+++ src/components/ComingSoon/ComingSoon.styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledComingSoonContainer = styled.div`
   position: relative;
   display: flex;
-  height: 50vh;
+  height: 80vh;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -12,4 +12,12 @@ export const StyledComingSoonContainer = styled.div`
   color: ${({ theme }) => theme.palette.yellow[500]};
   text-shadow: ${({ theme }) => theme.palette.yellow[500]} 1px 0 12px;
   cursor: no-drop;
+
+  &:hover {
+    background: linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0));
+    -webkit-backdrop-filter: blur(2px);
+    backdrop-filter: blur(2px);
+    box-shadow: 0 0 100px 400px rgba(0, 0, 0, 0.1) inset;
+    transition: box-shadow 0.3s ease;
+  }
 `;
