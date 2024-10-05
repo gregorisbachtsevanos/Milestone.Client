8b0dc848c8d4cc4a34b305481e9f654b75a6f86c
GregorisB <gregorbachts@gmail.com>
2024-10-05 22:09:40 +0300

    import the font family

--- src/assets/styles/global.styled.ts
+++ src/assets/styles/global.styled.ts
@@ -1,67 +1,86 @@
 import { createGlobalStyle } from "styled-components";
+import { fonts } from "./fonts.styled";
 
 export const GlobalStyles = createGlobalStyle`
-html{
-  overflow: hidden;
-  width: 100%; 
-}
-body {
-  padding: 0;
-  margin: 0;
-  font-family: 'Space Grotesk', sans-serif;
-  height: 100%;
-  width: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  overflow-y: hidden;
-  overflow-x: hidden;
-  -webkit-overflow-scrolling: touch;
-  color: ${({ theme }) => theme.palette.white[900]};
-  scrollbar-color: transparent ${({ theme }) => theme.palette.black[900]};
-}
-a {
-  color: inherit;
-  text-decoration: none;
-}
+  ${fonts
+    .map(
+      ({ weight, style, src }) => `
+    @font-face {
+      font-family: 'Spectral';
+      font-weight: ${weight};
+      font-style: ${style};
+      font-display: swap;
+      src: url(${src}) format('truetype');
+    }
+  `
+    )
+    .join("")}
 
-button{
-  padding: 0;
-  margin: 0;
-  border: 0;
-  outline: 0;
-  font-family: 'Space Grotesk', sans-serif;
-  color: orange;
-  background-color: transparent;
-  cursor: pointer;
-}
+  /* General Global Styles */
+  html {
+    overflow: hidden;
+    width: 100%;
+  }
 
-.transition-effect{
-  overflow: hidden;
-}
+  body {
+    padding: 0;
+    margin: 0;
+    font-family:  sans-serif;
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    overflow-y: hidden;
+    overflow-x: hidden;
+    -webkit-overflow-scrolling: touch;
+    color: ${({ theme }) => theme.palette.white[900]};
+    scrollbar-color: transparent ${({ theme }) => theme.palette.black[900]};
+  }
 
-* {
-  box-sizing: border-box;
-}
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
 
-@media (min-width: 500px) {
-      /* Chrome, Edge, and Safari */
-      *::-webkit-scrollbar {
+  button {
+    padding: 0;
+    margin: 0;
+    border: 0;
+    outline: 0;
+    font-family: sans-serif;
+    color: orange;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  .transition-effect {
+    overflow: hidden;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  /* Scrollbar Styling */
+  @media (min-width: 500px) {
+    /* Chrome, Edge, and Safari */
+    *::-webkit-scrollbar {
       width: 10px;
       height: 10px;
-  }
+    }
 
-  *::-webkit-scrollbar-track {
+    *::-webkit-scrollbar-track {
       background: ${({ theme }) => theme.palette.white[900]};
       border-radius: 5px;
-  }
+    }
 
-  *::-webkit-scrollbar-thumb {
-      background-color:red;
+    *::-webkit-scrollbar-thumb {
+      background-color: red;
       border-radius: 5px;
       border: 0px none ${({ theme }) => theme.palette.white[900]};
+    }
   }
-}
-
-
 `;
+
+export default GlobalStyles;
