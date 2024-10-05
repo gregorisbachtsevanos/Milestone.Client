fe8176c5bf8053b041689eea8fe25532d2e72484
GregorisB <gregorbachts@gmail.com>
2024-10-05 22:09:22 +0300

    global font family

---
+++ src/assets/styles/fonts.styled.ts
@@ -0,0 +1,31 @@
+import SpectralBold from "../fonts/Spectral-Bold.ttf";
+import SpectralBoldItalic from "../fonts/Spectral-BoldItalic.ttf";
+import SpectralExtraBold from "../fonts/Spectral-ExtraBold.ttf";
+import SpectralExtraBoldItalic from "../fonts/Spectral-ExtraBoldItalic.ttf";
+import SpectralExtraLight from "../fonts/Spectral-ExtraLight.ttf";
+import SpectralExtraLightItalic from "../fonts/Spectral-ExtraLightItalic.ttf";
+import SpectralItalic from "../fonts/Spectral-Italic.ttf";
+import SpectralLight from "../fonts/Spectral-Light.ttf";
+import SpectralLightItalic from "../fonts/Spectral-LightItalic.ttf";
+import SpectralMedium from "../fonts/Spectral-Medium.ttf";
+import SpectralMediumItalic from "../fonts/Spectral-MediumItalic.ttf";
+import SpectralRegular from "../fonts/Spectral-Regular.ttf";
+import SpectralSemiBold from "../fonts/Spectral-SemiBold.ttf";
+import SpectralSemiBoldItalic from "../fonts/Spectral-SemiBoldItalic.ttf";
+
+export const fonts = [
+  { weight: "normal", style: "normal", src: SpectralRegular },
+  { weight: "normal", style: "italic", src: SpectralItalic },
+  { weight: "bold", style: "normal", src: SpectralBold },
+  { weight: "bold", style: "italic", src: SpectralBoldItalic },
+  { weight: 500, style: "normal", src: SpectralMedium },
+  { weight: 500, style: "italic", src: SpectralMediumItalic },
+  { weight: 600, style: "normal", src: SpectralSemiBold },
+  { weight: 600, style: "italic", src: SpectralSemiBoldItalic },
+  { weight: 800, style: "normal", src: SpectralExtraBold },
+  { weight: 800, style: "italic", src: SpectralExtraBoldItalic },
+  { weight: 300, style: "normal", src: SpectralLight },
+  { weight: 300, style: "italic", src: SpectralLightItalic },
+  { weight: 200, style: "normal", src: SpectralExtraLight },
+  { weight: 200, style: "italic", src: SpectralExtraLightItalic },
+];
