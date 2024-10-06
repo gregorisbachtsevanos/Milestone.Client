bebd296fe539ed5747c515979ec55da749aa48e9
GregorisB <gregorbachts@gmail.com>
2024-10-06 21:58:12 +0300

    dlt files

--- src/features/profile/components/Settings/Settings.tsx
+++
@@ -1,7 +0,0 @@
-import { StyledSettingsContainer } from "./Settings.styled";
-
-const Settings = () => {
-  return <StyledSettingsContainer></StyledSettingsContainer>;
-};
-
-export default Settings;
