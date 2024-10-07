0e94d1acb880dbf466dc9e52bda8dadf6260e132
GregorisB <gregorbachts@gmail.com>
2024-10-07 19:55:56 +0300

    dlt files

--- src/features/profile/components/Edit/Edit.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledEditContainer = styled.div`
-  padding: 0 10px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-`;
