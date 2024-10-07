3f155c4d274ee4fed599d8c3b60738ded0c0862a
GregorisB <gregorbachts@gmail.com>
2024-10-07 19:56:14 +0300

    add mask css

--- src/features/profile/Profile.styled.ts
+++ src/features/profile/Profile.styled.ts
@@ -3,19 +3,11 @@ import { DefaultTheme } from "styled-components/dist/types";
 const black800 = ({ theme }: { theme: DefaultTheme }) => theme.palette.black[800];
 
 export const StyledProfileController = styled.div`
-  z-index: 999;
-  padding: 25px 20px;
   position: absolute;
   top: 10%;
   left: 78%;
   width: 400px;
-  background-color: ${black800};
   border-radius: 15px;
-  box-shadow: 0 5px 20px ${({ theme }) => theme.palette.black[900]};
-  display: flex;
-  flex-direction: column;
-  gap: 15px;
-  overflow: hidden;
 
   &::before {
     content: "";
@@ -24,5 +16,21 @@ export const StyledProfileController = styled.div`
     left: 85%;
     border: 10px solid transparent;
     border-bottom-color: ${black800};
+  }
+
+  .mask {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    overflow: hidden;
+    left: 0%;
+    top: 0%;
+    width: 400px;
+    gap: 15px;
+    padding: 25px 20px;
+    border-radius: 15px;
+    background-color: ${black800};
+    box-shadow: 0 5px 20px ${({ theme }) => theme.palette.black[900]};
+    z-index: 999;
   }
 `;
