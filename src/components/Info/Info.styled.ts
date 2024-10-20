f6082d52694e979149057febff8f8c1e156af806
GregorisB <gregorbachts@gmail.com>
2024-10-20 14:38:56 +0300

    dlt files

--- src/components/Info/Info.styled.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const StyledInfoContainer = styled.div`
-  max-height: 24px;
-  color: ${({ theme }) => theme.palette.black[500]};
-`;
--- src/utils/helpers.ts
+++
@@ -1,4 +0,0 @@
-export const getRandomValues = (count: number, min: number, max: number) =>
-  Array.from({ length: count }, () => Math.random() * (max - min) + min);
-
-export const normalizePathname = (path: string) => path.replace(/^\//, "").toLocaleLowerCase();
