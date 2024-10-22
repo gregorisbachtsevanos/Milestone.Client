037154762fb7f75f26f28a7e77b07ef54fb9aca0
GregorisB <gregorbachts@gmail.com>
2024-10-22 23:13:56 +0300

    change navbar dir

---
+++ src/features/overview/components/Tasks/components/Navbar/Navbar.tsx
@@ -0,0 +1,48 @@
+import Tags from "@/common/components/Tags";
+import useTaskCounts from "@/common/hooks/useTaskCounts";
+import { Status } from "@/types";
+import { useCallback, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import CreateNewButton from "../../../CreateNewButton";
+import NewTaskModal from "../NewTaskModal";
+import { StyledNavbarContainer } from "./Navbar.styled";
+
+const Navbar = () => {
+  const [newTaskModal, setNewTaskModal] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navbarData = useTaskCounts();
+
+  const handleParams = useCallback(
+    (type: Status) => {
+      searchParams.set("status", type);
+      setSearchParams(searchParams);
+    },
+    [searchParams, setSearchParams]
+  );
+
+  const openNewTaskModal = useCallback(() => {
+    setNewTaskModal(true);
+  }, []);
+
+  return (
+    <StyledNavbarContainer>
+      <div className="categories">
+        {navbarData.map(({ label, count }) => (
+          <Tags
+            key={label}
+            variant="dark"
+            color="yellow"
+            pointer
+            onClick={() => handleParams(label as Status)}
+          >
+            {`${label} (${count})`}
+          </Tags>
+        ))}
+      </div>
+      <CreateNewButton title="New Task" onOpen={openNewTaskModal} />
+      <NewTaskModal isOpen={newTaskModal} onClose={() => setNewTaskModal(false)} />
+    </StyledNavbarContainer>
+  );
+};
+
+export default Navbar;
