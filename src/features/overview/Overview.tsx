3d86281190e4c21a96dbd871d058f78e8182d2eb
GregorisB <gregorbachts@gmail.com>
2024-10-22 23:14:43 +0300

    add create new item modals

--- src/features/overview/Overview.tsx
+++ src/features/overview/Overview.tsx
@@ -6,21 +6,43 @@ import Notes from "./components/Notes";
 import Summary from "./components/Summary";
 import Tasks from "./components/Tasks";
 import { StyledOverviewContainer } from "./Overview.styled";
+import NewEventModal from "./components/Notes/components/NewEventModal";
+import NewGoalModal from "./components/Notes/components/NewGoalModal";
+import { useCallback, useState } from "react";
 
 const Overview = () => {
+  const [newGoalModal, setNewGoalModal] = useState(false);
+  const [newEventModal, setNewEventModal] = useState(false);
+
+  const openNewGoalModal = useCallback(() => {
+    setNewGoalModal(true);
+  }, []);
+
+  const openNewEventModal = useCallback(() => {
+    setNewGoalModal(true);
+  }, []);
+
   return (
     <StyledMainLayout>
       <StyledOverviewContainer>
         <Summary />
         <div className="sections">
           <Tasks />
-          <Notes data={notesData} title="Goals" />
+          <Notes data={notesData} title="Goals" btnTitle="New Goal" onBtnClick={openNewGoalModal} />
           <div className="row">
-            <Notes data={calendarData} title="Calendar" size={"small"} />
+            <Notes
+              data={calendarData}
+              title="Calendar"
+              size="small"
+              btnTitle="New Event"
+              onBtnClick={openNewEventModal}
+            />
             <Chat />
           </div>
         </div>
       </StyledOverviewContainer>
+      <NewEventModal isOpen={newEventModal} onClose={() => setNewEventModal(false)} />
+      <NewGoalModal isOpen={newGoalModal} onClose={() => setNewGoalModal(false)} />
     </StyledMainLayout>
   );
 };
