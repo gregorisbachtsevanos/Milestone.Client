3e44d30699c05446289f4c181c9630dc240f7ff5
GregorisB <gregorbachts@gmail.com>
2024-10-29 20:43:37 +0200

    change reminder to emailNotification on newEventValidationSchema

--- src/features/overview/validation/newEventValidationSchema.ts
+++ src/features/overview/validation/newEventValidationSchema.ts
@@ -4,6 +4,6 @@ export const newEventValidationSchema = yup.object().shape({
   title: yup.string().required("Task title is required"),
   description: yup.string().optional(),
   date: yup.string().required("Provide datetime"),
-  reminder: yup.boolean(),
+  emailNotification: yup.boolean(),
   tags: yup.array().optional(),
 });
