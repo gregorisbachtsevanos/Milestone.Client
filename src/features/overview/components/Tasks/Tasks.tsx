6097f77c76d6ff27d6e961714582c2754f2e9ff7
GregorisB <gregorbachts@gmail.com>
2024-10-31 20:23:49 +0200

    task components changes

--- src/features/overview/components/Tasks/Tasks.tsx
+++ src/features/overview/components/Tasks/Tasks.tsx
@@ -1,8 +1,9 @@
 import { useGetAllTasksQuery } from "@/app/services/projectManegerApi";
 import { Status } from "@/types";
 import { useSearchParams } from "react-router-dom";
-import Cards from "../Cards";
 import { StyledTasksContainer } from "./Tasks.styled";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
 
 const Tasks = () => {
   const [searchParams] = useSearchParams();
@@ -11,11 +12,12 @@ const Tasks = () => {
 
   const { data: tasks, isLoading } = useGetAllTasksQuery({ status: selectedTaskStatus as Status });
 
-  if (isLoading) return "loading";
-
   return (
     <StyledTasksContainer>
-      <Cards data={tasks ?? []} />
+      <Navbar />
+      <div className="cards-container">
+        <Card tasks={tasks} isLoading={isLoading} />
+      </div>
     </StyledTasksContainer>
   );
 };
