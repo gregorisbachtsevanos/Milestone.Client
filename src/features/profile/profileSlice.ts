983ae49f706767c468c3161546a7ed4e1ed4c830
GregorisB <gregorbachts@gmail.com>
2024-11-03 18:53:02 +0200

    add slice for current user and overview data

--- src/features/profile/profileSlice.ts
+++ src/features/profile/profileSlice.ts
@@ -1,22 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "app/store";
+import { Overview, UserType } from "./types";
 
-const initialState = {
+interface CachedDataState {
+  isOpen: boolean;
+  currentUser: UserType | null;
+  overview: Overview | null;
+}
+
+const initialState: CachedDataState = {
   isOpen: false,
+  currentUser: null,
+  overview: null,
 };
 
 const profileSlice = createSlice({
   name: "profile",
   initialState,
   reducers: {
-    setIsOpen(state, action) {
+    setIsOpen(state, action: PayloadAction<boolean>) {
       state.isOpen = action.payload;
+    },
+    setCurrentUser(state, action: PayloadAction<UserType>) {
+      state.currentUser = action.payload;
+    },
+    setOverview(state, action: PayloadAction<Overview>) {
+      state.overview = action.payload;
     },
   },
 });
 
-export const { setIsOpen } = profileSlice.actions;
+export const { setIsOpen, setCurrentUser, setOverview } = profileSlice.actions;
 
 export default profileSlice.reducer;
 
 export const isProfileModalOpen = (state: RootState) => state.profile.isOpen;
+export const getCurrentUser = (state: RootState) => state.profile.currentUser;
+export const overview = (state: RootState) => state.profile.overview;
