b3cef4918c725211a8e268f8761ab14bf3aeb57f
GregorisB <gregorbachts@gmail.com>
2024-11-03 18:53:20 +0200

    add types

--- src/app/services/userApi.ts
+++ src/app/services/userApi.ts
@@ -1,11 +1,12 @@
 import config from "@/.config/config";
 import { api } from "./api";
+import { UserType } from "@/features/profile/types";
 
 const { userAPI: USER_API } = config;
 
 export const userAPI = api.injectEndpoints({
   endpoints: (build) => ({
-    getCurrentUser: build.query<any, { user_id: string }>({
+    getCurrentUser: build.query<UserType, { user_id: string }>({
       query: ({ user_id }) => ({
         url: `${USER_API}/v1/users/me`,
         params: { user_id },
