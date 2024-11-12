4fea794790c9438f9438405d1be8be009c6dad3d
GregorisB <gregorbachts@gmail.com>
2024-11-12 20:38:42 +0200

    optimize useTaskCounts hook

--- src/common/hooks/useTaskCounts.ts
+++ src/common/hooks/useTaskCounts.ts
@@ -3,23 +3,16 @@ import { Status } from "@/types";
 import { useMemo } from "react";
 
 const useTaskCounts = (overviewTotalData?: Overview) => {
-  const counts = useMemo(() => {
+  return useMemo(() => {
     const { totalSubtasks } = overviewTotalData ?? {};
 
-    return {
-      backlog: totalSubtasks?.backlog ?? 0,
-      onHold: totalSubtasks?.onHold ?? 0,
-      inProgress: totalSubtasks?.inProgress ?? 0,
-      completed: totalSubtasks?.completed ?? 0,
-    };
+    return [
+      { label: Status.Backlog, count: totalSubtasks?.backlog ?? 0 },
+      { label: Status.OnHold, count: totalSubtasks?.onHold ?? 0 },
+      { label: Status.InProgress, count: totalSubtasks?.inProgress ?? 0 },
+      { label: Status.Completed, count: totalSubtasks?.completed ?? 0 },
+    ];
   }, [overviewTotalData]);
-
-  return [
-    { label: Status.Backlog, count: counts.backlog },
-    { label: Status.OnHold, count: counts.onHold },
-    { label: Status.InProgress, count: counts.inProgress },
-    { label: Status.Completed, count: counts.completed },
-  ];
 };
 
 export default useTaskCounts;
