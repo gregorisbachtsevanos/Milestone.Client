4dfaba42d396bab69add2ad126e1e81b79b6f88e
GregorisB <gregorbachts@gmail.com>
2024-11-13 21:11:01 +0200

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import ComingSoon from "@/common/components/ComingSoon";
 import AuthLayout from "@/layouts/AuthLayout";
 import AppLayout from "layouts/AppLayout";
 import DashboardLayout from "layouts/DashboardLayout";
 import { Navigate, Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import PublicRoutes from "routes/PublicRoutes";
-import ComingSoon from "@/common/components/ComingSoon";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
 import Overview from "./features/overview";
