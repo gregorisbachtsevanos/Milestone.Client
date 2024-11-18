962479c4611d278079889e4131a62183220d0b2e
GregorisB <gregorbachts@gmail.com>
2024-11-18 02:29:34 +0200

    change default value for dealine

--- src/features/overview/hooks/useInitNewProjectForm.ts
+++ src/features/overview/hooks/useInitNewProjectForm.ts
@@ -1,14 +1,14 @@
+import { PriorityOptions, StatusOptions } from "@/features/overview/types";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import { newProjectValidationSchema } from "../validation/newProjectValidationSchema";
-import { PriorityOptions, StatusOptions } from "@/features/overview/types";
 
 const useInitNewProjectForm = () => {
   const methods = useForm({
     defaultValues: {
       name: "",
       description: "",
-      deadline: "",
+      deadline: new Date().toISOString(),
       status: {
         value: "backlog",
         label: StatusOptions.Backlog,
