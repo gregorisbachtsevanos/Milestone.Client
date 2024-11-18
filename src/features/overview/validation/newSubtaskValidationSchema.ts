7141a1dc71711ad78fce4dad9ebb866f1fa143da
GregorisB <gregorbachts@gmail.com>
2024-11-19 00:39:04 +0200

    add new subtask validation

---
+++ src/features/overview/validation/newSubtaskValidationSchema.ts
@@ -0,0 +1,22 @@
+import * as yup from "yup";
+
+export const newSubtaskValidationSchema = yup.object().shape({
+  name: yup.string().required("Project name is required"),
+  description: yup.string().optional(),
+  deadline: yup.string().required("Provide deadline"),
+  status: yup
+    .object()
+    .shape({
+      value: yup.string().required("Please provide status"),
+      label: yup.string().required("Status label is required"),
+    })
+    .required("Please provide status"),
+  priority: yup
+    .object()
+    .shape({
+      value: yup.string().required("Please provide priority"),
+      label: yup.string().required("Priority label is required"),
+    })
+    .required("Please provide priority"),
+  tags: yup.array().optional(),
+});
