1d9a2b306f118dee13bea3333ab4f01f5707335a
GregorisB <gregorbachts@gmail.com>
2024-11-19 00:39:56 +0200

    init new subtask form hook

---
+++ src/features/overview/hooks/useInitNewSubtaskForm.ts
@@ -0,0 +1,29 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { newSubtaskValidationSchema } from "../validation/newSubtaskValidationSchema";
+import { PriorityOptions, StatusOptions } from "../types";
+
+const useInitNewSubtaskForm = () => {
+  const methods = useForm({
+    defaultValues: {
+      name: "",
+      description: "",
+      deadline: new Date().toISOString(),
+      status: {
+        value: "backlog",
+        label: StatusOptions.Backlog,
+      },
+      priority: {
+        value: "low",
+        label: PriorityOptions.Low,
+      },
+      tags: [],
+    },
+    mode: "onBlur",
+    resolver: yupResolver(newSubtaskValidationSchema),
+  });
+
+  return { methods };
+};
+
+export default useInitNewSubtaskForm;
