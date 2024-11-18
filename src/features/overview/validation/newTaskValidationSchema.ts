9e468a3b5813ade137b300df70814eae239d234c
GregorisB <gregorbachts@gmail.com>
2024-11-19 00:38:55 +0200

    add new task validation

---
+++ src/features/overview/validation/newTaskValidationSchema.ts
@@ -0,0 +1,29 @@
+import * as yup from "yup";
+
+export const newTaskValidationSchema = yup.object().shape({
+  projectName: yup
+    .object()
+    .shape({
+      value: yup.string().required("Please provide status"),
+      label: yup.string().required("Status label is required"),
+    })
+    .required("Please select a project to associate"),
+  name: yup.string().required("Task title is required"),
+  description: yup.string().optional(),
+  deadline: yup.string().required("Provide deadline"),
+  status: yup
+    .object()
+    .shape({
+      value: yup.string().required("Please provide status"),
+      label: yup.string().required("Status label is required"),
+    })
+    .required("Please provide status"),
+  priority: yup
+    .object()
+    .shape({
+      value: yup.string().required("Please provide priority"),
+      label: yup.string().required("Priority label is required"),
+    })
+    .required("Please provide priority"),
+  tags: yup.array().optional(),
+});
